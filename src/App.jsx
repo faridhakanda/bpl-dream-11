9d423bd1aacf9028b1e25cc7b31fa9434b1fad11
faridhakanda <faridakanda1729@gmail.com>
2026-03-26 10:04:55 +0600

    first commit after project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+        <button className='btn btn-outline btn-primary'>Click Me</button>
+        <h2 className='text-2xl text-pink-500 text-center'>BPL Dream 11 Project!</h2>
+        <p className='text-lime-300 text-2xl p-4 border-red-500 border-1 m-4 rounded-md'>BPL Dream 11 project is ready to push github</p>
+    
+    </div>
+  )
+}
+
+export default App
